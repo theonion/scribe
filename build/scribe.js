e820347e8ddb290b2637e2c120047be16b600719
Adam Wentz <awentz@theonion.com>
2014-08-19 14:43:30 -0500

    Fix for safari paste

--- build/scribe.js
+++ build/scribe.js
@@ -2498,12 +2498,17 @@ define('plugins/core/events',[
        * I also don't like how it has the authority to perform `event.preventDefault`.
        */
 
+      function agentIsSafari() {
+        var ua = navigator.userAgent;
+        return ua.search('Safari') >= 0 && ua.search('Chrome') < 0;
+      }
+
       scribe.el.addEventListener('paste', function handlePaste(event) {
         /**
          * Browsers without the Clipboard API (specifically `ClipboardEvent.clipboardData`)
-         * will execute the second branch here.
+         * and Safari will execute the second branch here.
          */
-        if (event.clipboardData) {
+        if (event.clipboardData && !agentIsSafari()) {
           event.preventDefault();
 
           if (contains(event.clipboardData.types, 'text/html')) {
