9d877148c694b7d7a20fc10dfd57c74b9600c8ce
Chris Sinchok <csinchok@theonion.com>
2014-09-02 16:01:04 -0500

    New build

--- build/scribe.js
+++ build/scribe.js
@@ -3089,36 +3089,6 @@ define('plugins/core/inline-elements-mode',[],function () {
   };
 });
 
-define('plugins/core/patches/commands/bold',[],function () {
-
-  
-
-  return function () {
-    return function (scribe) {
-      var boldCommand = new scribe.api.CommandPatch('bold');
-
-      /**
-       * Chrome: Executing the bold command inside a heading corrupts the markup.
-       * Disabling for now.
-       */
-      boldCommand.queryEnabled = function () {
-        var selection = new scribe.api.Selection();
-        var headingNode = selection.getContaining(function (node) {
-          return (/^(H[1-6])$/).test(node.nodeName);
-        });
-
-        return scribe.api.CommandPatch.prototype.queryEnabled.apply(this, arguments) && ! headingNode;
-      };
-
-      // TODO: We can't use STRONGs because this would mean we have to
-      // re-implement the `queryState` command, which would be difficult.
-
-      scribe.commandPatches.bold = boldCommand;
-    };
-  };
-
-});
-
 define('plugins/core/patches/commands/indent',[],function () {
 
   /**
@@ -3583,7 +3553,6 @@ define('plugins/core/patches/events',['scribe-common/src/element'], function (el
 });
 
 define('plugins/core/patches',[
-  './patches/commands/bold',
   './patches/commands/indent',
   './patches/commands/insert-html',
   './patches/commands/insert-list',
@@ -3591,7 +3560,6 @@ define('plugins/core/patches',[
   './patches/commands/create-link',
   './patches/events'
 ], function (
-  boldCommand,
   indentCommand,
   insertHTMLCommand,
   insertListCommands,
@@ -3610,7 +3578,6 @@ define('plugins/core/patches',[
 
   return {
     commands: {
-      bold: boldCommand,
       indent: indentCommand,
       insertHTML: insertHTMLCommand,
       insertList: insertListCommands,
@@ -4369,7 +4336,6 @@ define('scribe',[
 
 
     // Patches
-    this.use(patches.commands.bold());
     this.use(patches.commands.indent());
     this.use(patches.commands.insertHTML());
     this.use(patches.commands.insertList());
